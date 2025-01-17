4ad16444f994cafaa85740c0ad2ab5e3cc974109
MahimaJha2811 <mahimajha0811@gmail.com>
2025-01-17 21:21:38 +0530

    added submit features

--- practice/src/components/Form.jsx
+++ practice/src/components/Form.jsx
@@ -3,15 +3,29 @@ export default function Form() {
   const ingredientsList = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
+
+  function handleSubmit(event) {
+    // is se page refresh nahi hota
+    event.preventDefault();
+    console.log("Form Submitted");
+
+    //need to learn about current target
+    const formdata = new FormData(event.currentTarget);
+    const newIngredients = formdata.get("ingredient");
+    console.log(newIngredients);
+    console.log(newIngredients);
+  }
   return (
     <>
       <main>
-        <form className="addForm">
+        <form onSubmit={handleSubmit} className="addForm">
           <input
             className="textbar"
             type="text"
             placeholder="tomato"
             aria-label="Add ingredient"
+            // this name element will be shown in the url
+            name="ingredient"
           />
           <button className="addBtn">+ Add ingrident</button>
         </form>
